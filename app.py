d8a128af2de4ce82389f59724dd87ce3e50321b7
ayush kumar mishra <ayushkumarmishra000@gmail.com>
2024-09-08 16:11:41 +0530

    added file add option

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os
+from flask import request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -14,5 +17,36 @@ def model():
 def dataset():
     return render_template('dataset.html')
 
+@app.route('/find')
+def find():
+    return render_template('find.html')
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'doc', 'docx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('result.html')
+    return 'Invalid file type'
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
